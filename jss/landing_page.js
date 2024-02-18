37f57bf43cff19c99a3a9cc37d37c965a26a7a14
Sabareesh T <Sabareesh.cs22@bitsathy.ac.in>
2024-02-18 22:14:37 +0530

    added image slider

---
+++ jss/landing_page.js
@@ -0,0 +1,44 @@
+const slides = document.querySelectorAll(".slider");
+var counter =0;
+
+var no_of_images = 0;
+slides.forEach(
+    (slide,index)=>{
+        no_of_images++
+        slide.style.left = String(index*100)+'%';
+
+        console.log(no_of_images)
+    }
+)
+const goPrev = ()=>{
+    if(counter<=((no_of_images-1)*-1)){
+        counter=0;
+    }
+    counter--
+    console.log(counter)
+    slideImage()
+}
+const goNext = ()=>{
+    if(counter>=0){
+        counter = (no_of_images-1)*-1
+    }
+        counter++
+        console.log(counter)
+        slideImage()
+}
+
+
+const slideImage = () => {
+    slides.forEach(
+        (slide)=>{
+            slide.style.transform = 'translateX'+'('+String(counter*100)+'%'+')'
+        }
+    )
+}
+const auto = ()=>{
+    while(true){
+        setTimeout(
+            goNext()
+          ,10000)
+    }
+}
