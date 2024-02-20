42479a858e81cdda0ba7f9effc22eea1aeccb699
Sabareesh T <Sabareesh.cs22@bitsathy.ac.in>
2024-02-20 17:01:13 +0530

    made app.py work

--- app.py
+++ app.py
@@ -1,78 +1,104 @@
-from flask import Flask , render_template ,redirect
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import InputRequired,Length,ValidationError
-from flask_login import UserMixin , LoginManager,logout_user,login_required,login_user,current_user
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_login import UserMixin, LoginManager, logout_user, login_required, login_user, current_user
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-app.config['SECRET_KEY'] ="this is a secret key"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "this is a secret key"
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 app.app_context().push()
-
-login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 
-class User(db.Model,UserMixin):
-    id = db.Column(db.Integer,primary_key=True)
-    firstname = db.Column(db.String(20),nullable=False)
-    lastname = db.Column(db.String(20),nullable=False)
-    username = db.Column(db.String(20),unique=True,nullable=False)
-    email = db.Column(db.String(20),unique=True)
-    password = db.Column(db.String(120),nullable=False)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(20), nullable=False)
+    lastname = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(120), nullable=False)
+
 
 class RegistrationForm(FlaskForm):
-    firstname = StringField('First Name',validators=[InputRequired(),Length(min=4,max=20)])
-    lastname = StringField('Last Name',validators=[InputRequired(),Length(min=1,max=20)])
-    username = StringField('Username',validators=[InputRequired(),Length(min=4,max=20)])
-    email = StringField('Email',validators=[InputRequired(),Length(min=8,max=40)])
-    password = PasswordField('Password',validators=[InputRequired(),Length(min=8,max=20)])
+    firstname = StringField('First Name', validators=[InputRequired(), Length(min=4, max=20)])
+    lastname = StringField('Last Name', validators=[InputRequired(), Length(min=1, max=20)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[InputRequired(), Length(min=8, max=40)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=20)])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self,username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('Username already taken')
-    def validate_email(self,email):
+
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('Email already taken')
+
+
 class LoginForm(FlaskForm):
-    username = StringField('Username',validators=[InputRequired(),Length(min=4,max=20)])
-    password = PasswordField('Password',validators=[InputRequired(),Length(min=8,max=20)])
-    submit = SubmitField('Login')  
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=20)])
+    submit = SubmitField('Login')
+
+
 @app.route('/')
 def home():
     return render_template('landing_page.html')
 
 
-@app.route('/login.html',methods=['GET','POST'])
+@app.route('/login.html', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', form = form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('profile'))
+        else:
+            flash('Login failed. Please check your username and password.', 'danger')
+    return render_template('login.html', form=form)
 
 
-@app.route('/signup.html', methods=['GET','POST'])
+@app.route('/signup.html', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm()
-    
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(firstname = form.firstname.data,lastname = form.lastname.data,username = form.username.data,email = form.email.data,password = hashed_password)
+        user = User(firstname=form.firstname.data, lastname=form.lastname.data, username=form.username.data,
+                    email=form.email.data, password=hashed_password)
         db.session.add(user)
-        db.session.commit()
-        return redirect('/login.html')
-        return '<h1>Account created for {}!</h1>'.format(form.username.data)
-    return render_template('signup.html',form = form)
+        try:
+            db.session.commit()
+            flash('Account created successfully. You can now log in.', 'success')
+            return redirect(url_for('login'))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'An error occurred: {str(e)}', 'danger')
+            return redirect(url_for('signup'))
+    return render_template('signup.html', form=form)
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', user=current_user)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
